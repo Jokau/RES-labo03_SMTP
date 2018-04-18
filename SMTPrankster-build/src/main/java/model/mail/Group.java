5eeab13c192afb59d0de81619d6690eb0a935524
Joël Kaufmann <jolauka@gmail.com>
2018-04-19 00:58:48 +0200

    add models draft

--- SMTPrankster-build/src/main/java/model/mail/Group.java
+++ SMTPrankster-build/src/main/java/model/mail/Group.java
@@ -1,4 +1,59 @@
 package model.mail;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Group Model
+ *
+ * A Group is a group of Persons.
+ *
+ * The number n of Persons in a group is define in /config/config.properties.
+ * Within a group, one person is going to be the sender and the other are the receivers.
+ *
+ * @author Joël Kaufmann (Jokau), Loïc Schürch (loic-schurch)
+ */
 public class Group {
+
+    private Person sender;                                      // Person that is the sender of the prank
+    private List<Person> receivers = new ArrayList<Person>();   // List of all receivers
+
+
+    /**
+     * Constructor of a Group.
+     * @param sender the Person whose e-mail address is used as sender
+     * @param receivers the Persons whose e-mail addresses are used as receivers
+     */
+    public Group(Person sender, List<Person> receivers) {
+        this.sender = sender;
+        this.receivers.addAll(receivers);
+    }
+
+    /**
+     * Constructor of a Group.
+     * @param sender the Person whose e-mail address is used as sender
+     * @param receivers the Persons whose e-mail addresses are used as receivers
+     */
+    public Group(Person sender, Person... receivers) {
+        this.sender = sender;
+        for (Person receiver : receivers) {
+            this.receivers.add(receiver);
+        }
+    }
+
+    /**
+     * Getter of sender.
+     * @return the sender
+     */
+    public Person getSender() {
+        return sender;
+    }
+
+    /**
+     * Getter of the receivers.
+     * @return the receivers
+     */
+    public List<Person> getReceivers() {
+        return receivers;
+    }
 }
--- SMTPrankster-build/src/main/java/model/mail/Person.java
+++ SMTPrankster-build/src/main/java/model/mail/Person.java
@@ -1,4 +1,35 @@
 package model.mail;
 
+import com.sun.istack.internal.NotNull;
+
+/**
+ * Person Model
+ *
+ * A Person is defined by their e-mail address.
+ * A Person can be a sender victim or a receiver victim, no distinction is operated.
+ *
+ * A sender is the one whose e-mail address is used as the sender in the prank e-mail
+ * and a receiver if someone that will receive the prank e-mail.
+ *
+ * @author Joël Kaufmann (Jokau), Loïc Schürch (loic-schurch)
+ */
 public class Person {
+    private String emailAddress;    //e-mail address
+
+    public Person(@NotNull String emailAddress) throws IllegalArgumentException {
+        String pattern = "\\w+@\\w+\\.\\w+";
+        if (emailAddress.matches(pattern)) {
+            this.emailAddress = emailAddress;
+        } else {
+            throw  new IllegalArgumentException("valid address required");
+        }
+    }
+
+    /**
+     * Getter of the e-mail address
+     * @return the Person's e-mail address
+     */
+    public String getEmailAddress() {
+        return emailAddress;
+    }
 }
