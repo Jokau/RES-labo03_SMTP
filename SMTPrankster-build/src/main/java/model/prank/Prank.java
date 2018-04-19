82b965f81d96dd5b0c4fa2934582c8ef1f60056a
Joël Kaufmann <jolauka@gmail.com>
2018-04-19 02:19:31 +0200

    implementation of Prank

--- SMTPrankster-build/src/main/java/model/prank/Prank.java
+++ SMTPrankster-build/src/main/java/model/prank/Prank.java
@@ -1,4 +1,79 @@
 package model.prank;
 
+import com.sun.istack.internal.NotNull;
+import model.mail.Mail;
+import model.mail.Person;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Class that models a Prank
+ *
+ * A Prank is defined by its message to send, the sender and the receivers.
+ * It generates a Mail to send for each receiver.
+ *
+ * @author Joël Kaufmann (Jokau), Loïc Schürch (loic-schurch)
+ */
 public class Prank {
+    private Person sender;      // Sender
+    private List<Person> receivers = new LinkedList<Person>();  // receivers
+    private String message;     // content of the message to send
+
+    /**
+     * Prank constructor.
+     * @param sender sender of the prank
+     * @param receivers receivers of the prank
+     * @param message text to send
+     */
+    public Prank(@NotNull Person sender, @NotNull List<Person> receivers, @NotNull String message) {
+        this.message = message;
+        this.sender = sender;
+        this.receivers.addAll(receivers);
+    }
+
+    /**
+     * Prank constructor.
+     * @param sender sender of the prank
+     * @param message text to send
+     * @param receivers receivers of the prank
+     */
+    public Prank(@NotNull Person sender, @NotNull String message, @NotNull Person... receivers) {
+        this.message = message;
+        this.sender = sender;
+        for (Person person: receivers) {
+            this.receivers.add(person);
+        }
+    }
+
+    /**
+     * Getter of the sender.
+     * @return the sender
+     */
+    public Person getSender() {
+        return sender;
+    }
+
+    /**
+     * Getter of the receivers.
+     * @return a list that contains all the receivers
+     */
+    public List<Person> getReceivers() {
+        return receivers;
+    }
+
+    /**
+     * Create a mail for each receiver, as the Prank is addressed one by one.
+     * The messages created are ready to be sent.
+     *
+     * @return a list of Mails ready to be sent
+     */
+    public List<Mail> createMessages() {
+        ArrayList<Mail> mails = new ArrayList<Mail>();
+        for (Person receiver : receivers) {
+            mails.add(new Mail(sender, receiver, message));
+        }
+        return mails;
+    }
 }
--- SMTPrankster-build/src/main/java/model/prank/PrankGenerator.java
+++ SMTPrankster-build/src/main/java/model/prank/PrankGenerator.java
@@ -1,4 +1,17 @@
 package model.prank;
 
+/**
+ * Class that sets Pranks to Groups
+ *
+ * @author Joël Kaufmann (Jokau), Loïc Schürch (loic-schurch)
+ */
 public class PrankGenerator {
+
+    public PrankGenerator() {
+        //TODO get number of groups
+        //TODO create the exact number of groups
+        //TODO put Person in groups (at random?)
+        //TODO assign a Prank to each group
+        //TODO for each goup send mails
+    }
 }
