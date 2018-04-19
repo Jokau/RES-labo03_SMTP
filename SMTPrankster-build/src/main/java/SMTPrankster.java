7fd3a49ed32ec026c441b451b79ddf18bf18268c
Joël Kaufmann <jolauka@gmail.com>
2018-04-19 21:02:39 +0200

    handle main exception

--- SMTPrankster-build/src/main/java/SMTPrankster.java
+++ SMTPrankster-build/src/main/java/SMTPrankster.java
@@ -8,7 +8,11 @@ import smtp.SmtpClient;
 import java.io.IOException;
 import java.util.ArrayList;
 
+/**
+ * Program entry Class
+ */
 public class SMTPrankster {
+
     public static void main(String[] args) {
 
         ConfigurationManager manager = ConfigurationManager.getConfigManager();
@@ -16,24 +20,29 @@ public class SMTPrankster {
         try{
              generator = new PrankGenerator();
         } catch(NotEnoughVictimsException e) {
-            System.out.println("AH");
+            System.err.println(e.getMessage());
+            System.err.println("Please edit config/victims.utf8 or config/config.properties "
+                    + "to reach minimum of victims per group");
+            System.exit(1);
         }
 
         ArrayList<Prank> pranks = generator.getPranks();
 
+        SmtpClient client = null;
         try {
-            SmtpClient client = new SmtpClient(manager.getSmtpServerAddress(), manager.getSmtpPortNumber());
+            client = new SmtpClient(manager.getSmtpServerAddress(), manager.getSmtpPortNumber());
 
             for(Prank p: pranks) {
                 for(Mail mail: p.getMails()) {
                     client.sendMail(mail);
                 }
             }
-            client.close();
-
         } catch(IOException e) {
             e.printStackTrace();
+        } finally {
+            if (client != null) {
+                client.close();
+            }
         }
     }
-
 }
